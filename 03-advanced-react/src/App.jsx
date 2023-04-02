5f5f69bb401a787eb5cedef718d43b5b38c570d4
Nicolas Mayorga <namayorgav@gmail.com>
2023-04-02 14:50:42 -0500

    Progress until Project Structure

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,9 +1,20 @@
+import Navbar from "./tutorial/04-project-structure/starter/Navbar"
+import {
+  Home,
+  About,
+  Contact,
+} from "./tutorial/04-project-structure/starter/Pages"
+
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <h2>Advanced React</h2>
+      <Navbar />
+      <Home />
+      <About />
+      <Contact />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
