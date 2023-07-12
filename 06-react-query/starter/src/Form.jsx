f6dd66c892ee7dec3e7cccb2d7effa7a32f15fd1
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-12 14:14:24 -0500

    Learned all related to useQuery and useMutation.

--- 06-react-query/starter/src/Form.jsx
+++ 06-react-query/starter/src/Form.jsx
@@ -1,26 +1,39 @@
-import { useState } from 'react';
+import { useMutation } from "@tanstack/react-query"
+import { useState } from "react"
+import customFetch from "./utils"
 
 const Form = () => {
-  const [newItemName, setNewItemName] = useState('');
+  const [newItemName, setNewItemName] = useState("")
+
+  const { mutate: createTask, isLoading } = useMutation({
+    mutationFn: (taskTitle) =>
+      customFetch.post("/", {
+        title: taskTitle,
+      }),
+  })
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+    e.preventDefault()
+    createTask(newItemName)
+    setNewItemName("")
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <h4>task bud</h4>
-      <div className='form-control'>
+      <div className="form-control">
         <input
-          type='text '
-          className='form-input'
+          type="text "
+          className="form-input"
           value={newItemName}
           onChange={(event) => setNewItemName(event.target.value)}
+          required
         />
-        <button type='submit' className='btn'>
+        <button type="submit" className="btn" disabled={isLoading}>
           add task
         </button>
       </div>
     </form>
-  );
-};
-export default Form;
+  )
+}
+export default Form
--- 06-react-query/starter/src/Items.jsx
+++ 06-react-query/starter/src/Items.jsx
@@ -1,11 +1,27 @@
-import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import { useQuery } from "@tanstack/react-query"
+import SingleItem from "./SingleItem"
+import customFetch from "./utils"
+
+const Items = () => {
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: () => customFetch.get("/"),
+  })
+
+  if (isLoading) {
+    return <p style={{ marginTop: "1rem" }}>Loading...</p>
+  }
+
+  // if (isError) return <p style={{ marginTop: "1rem" }}>There was an error...</p>
+
+  if (error) return <p style={{ marginTop: "1rem" }}>{error.message}</p>
+
   return (
-    <div className='items'>
-      {items.map((item) => {
-        return <SingleItem key={item.id} item={item} />;
+    <div className="items">
+      {data.data.taskList.map((item) => {
+        return <SingleItem key={item.id} item={item} />
       })}
     </div>
-  );
-};
-export default Items;
+  )
+}
+export default Items
