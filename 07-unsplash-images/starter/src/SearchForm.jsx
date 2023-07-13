50c6668a9afb55b60e354c87028ee54b1a8dd920
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-13 17:06:36 -0500

    getting the value of the form through target.elemn

--- 07-unsplash-images/starter/src/SearchForm.jsx
+++ 07-unsplash-images/starter/src/SearchForm.jsx
@@ -1,5 +1,27 @@
 function SearchForm() {
-  return <div>SearchForm</div>
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const searchValue = e.target.elements.search.value
+  }
+
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="Cat"
+          autoComplete="off"
+          required
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  )
 }
 
 export default SearchForm
