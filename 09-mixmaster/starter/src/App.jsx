9f918e0d3960df1677d3c34dabddcbc79df4e6eb
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-30 10:08:32 -0500

    errorElement for rendering page of 404 not found.

--- 09-mixmaster/starter/src/App.jsx
+++ 09-mixmaster/starter/src/App.jsx
@@ -9,14 +9,18 @@ import {
   HomeLayout,
 } from "./Pages"
 
+import { loader as landingLoader } from "./Pages/Landing"
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />, // This parent will have the shared layout so it MUST have the <Outlet/> component
+    errorElement: <Error />,
     children: [
       {
         index: true,
         element: <Landing />,
+        loader: landingLoader,
       },
       {
         path: "cocktail",
