65760bb09d58987e7401fbf4f735851f42df93d6
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-30 10:09:20 -0500

    Handled the error through useRoutError().

--- 09-mixmaster/starter/src/Pages/Error.jsx
+++ 09-mixmaster/starter/src/Pages/Error.jsx
@@ -1,4 +1,30 @@
+import { Link, useRouteError } from "react-router-dom"
+import ErrorDiv from "../assets/wrappers/ErrorPage"
+import img from "../assets/not-found.svg"
+
 function Error() {
-  return <h1>Error</h1>
+  const error = useRouteError()
+  console.log(error)
+
+  if (error.status === 404) {
+    return (
+      <ErrorDiv>
+        <div>
+          <img src={img} alt="Error 404 image, not found" />
+          <h3>Ohh!</h3>
+          <p>We can't seem to find page you are looking for</p>
+          <Link to={"/"}>back home</Link>
+        </div>
+      </ErrorDiv>
+    )
+  }
+
+  return (
+    <ErrorDiv>
+      <div>
+        <h3>Something went wrong</h3>
+      </div>
+    </ErrorDiv>
+  )
 }
 export default Error
