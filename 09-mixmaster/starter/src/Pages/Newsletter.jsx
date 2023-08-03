40410b05c68148ce0dc338513f97bbf8cc9f1dfb
Nicolas Mayorga <namayorgav@gmail.com>
2023-08-02 20:11:42 -0500

    form setup.

--- 09-mixmaster/starter/src/Pages/Newsletter.jsx
+++ 09-mixmaster/starter/src/Pages/Newsletter.jsx
@@ -1,4 +1,56 @@
 function Newsletter() {
-  return <h1>Newsletter</h1>
+  return (
+    <form className="form">
+      <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+        our newsletter
+      </h4>
+      {/* name */}
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="form-input"
+          defaultValue={"jhon"}
+        />
+      </div>
+      {/* lastName */}
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          last name
+        </label>
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          className="form-input"
+          defaultValue={"smith"}
+        />
+      </div>
+      {/* email */}
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          className="form-input"
+          defaultValue={"js@test.com"}
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-block"
+        style={{ marginTop: ".5rem" }}
+      >
+        Submit
+      </button>
+    </form>
+  )
 }
 export default Newsletter
